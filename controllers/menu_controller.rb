b567463be459aaeff6ed91051bd6151454756611
Dave Lively <davelively14@gmail.com>
2017-02-04 17:26:18 -0500

    Assignment 6 complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "Main Menu - #{@address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View entry by number"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -25,17 +26,21 @@ class MenuController
         main_menu
       when 2
         system "clear"
-        create_entry
+        view_entry_by_number
         main_menu
       when 3
         system "clear"
-        search_entries
+        create_entry
         main_menu
       when 4
         system "clear"
-        read_csv
+        search_entries
         main_menu
       when 5
+        system "clear"
+        read_csv
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -75,6 +80,31 @@ class MenuController
         system "clear"
         puts "#{selection} is not a valid input"
         entry_submenu(entry)
+    end
+  end
+
+  def view_entry_by_number
+    print "Enter entry number, or 'm' to return to main menu: "
+    entry = gets.chomp
+
+    if entry.downcase == 'm'
+      system "clear"
+      main_menu
+      return
+    end
+
+    entry = entry.to_i
+
+    if address_book.entries[entry]
+      system "clear"
+      puts address_book.entries[entry]
+      print "Hit enter to return to main menu"
+      gets.chomp
+      system "clear"
+    else
+      system "clear"
+      puts "Not a valid index."
+      view_entry_by_number
     end
   end
 
